c538e82340f5de828364b823d313c0ec2087c166
Kevin Kuhls <kekuhls@cisco.com>
2016-07-12 13:13:40 -0700

    added vpn shell script

---
+++ vpn.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+case "$1" in
+
+1) echo "VPN v540user2"
+   printf "v540user2\nP5b227a\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+2) echo "VPN v540user3"
+   printf "v540user3\nP5b227a\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+3) echo "VPN v540user4"
+   printf "v540user4\nP5b227a\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+4) echo "VPN v655user2"
+   printf "v655user2\neff0bc\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+5) echo "VPN v655user3"
+   printf "v655user2\neff0bc\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+6) echo "VPN v655user4"
+   printf "v655user2\neff0bc\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+esac
+
