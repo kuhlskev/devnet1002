158f8020865a3bd6319de019d27cb5fd6ac08fde
Kevin Kuhls <kekuhls@cisco.com>
2016-07-13 06:30:30 -0700

    updated asa_iac and vpn script

--- vpn.sh
+++ vpn.sh
@@ -26,5 +26,25 @@ case "$1" in
    printf "v655user2\neff0bc\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
    ;;
 
+7) echo "VPN v499user1"
+   printf "v499user1\n6cecb9\ny" | /opt/cisco/anyconnect/bin/vpn -s connect dcloud-lon-anyconnect.cisco.com
+   ;;
+
+d) echo "VPN Discnnect"
+   /opt/cisco/anyconnect/bin/vpn -s disconnect
+   ;;
+
+dis) echo "VPN Discnnect"
+   /opt/cisco/anyconnect/bin/vpn -s disconnect
+   ;;
+
+disco) echo "VPN Discnnect"
+   /opt/cisco/anyconnect/bin/vpn -s disconnect
+   ;;
+
+disconnect) echo "VPN Discnnect"
+   /opt/cisco/anyconnect/bin/vpn -s disconnect 
+   ;;
+
 esac
 
