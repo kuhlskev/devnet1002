a5719474bec01e35c6521d51ef2c42559f4aedf3
Kevin Kuhls <kekuhls@cisco.com>
2018-04-10 12:23:32 -0400

    added nxos to iac

--- dnc2018/cmds.sh
+++ dnc2018/cmds.sh
@@ -6,28 +6,41 @@ case "$1" in
    ansible-playbook ios_example.yaml -i inventory
    ;;
 
-2) echo "ansible-playbook linux.yaml -i inventory"
-   ansible-playbook linux.yaml -i inventory
+2) echo "ansible-playbook linux.yaml"
+   ansible-playbook linux.yaml
    ;;
 
-3) echo "ansible-playbook nc_example.yaml -i inventory"
-   ansible-playbook nc_example.yaml -i inventory
+3) echo "ansible-playbook nc_example.yaml"
+   ansible-playbook nc_example.yaml
    ;;
 
-4) echo "ansible-playbook nxos.yaml -i inventory"
-   ansible-playbook nxos.yaml -i inventory
+4) echo "ansible-playbook nxos.yaml"
+   ansible-playbook nxos.yaml
    ;;
 
-5) echo "ansible-playbook aci.yaml -i inventory"
-   ansible-playbook aci.yaml -i inventory
+5) echo "ansible-playbook aci.yaml"
+   ansible-playbook aci.yaml
    ;;
 
-6) echo "ansible-playbook ucs.yaml -i inventory"
-   ansible-playbook ucs.yaml -i inventory
+6) echo "ansible-playbook ucs.yaml"
+   ansible-playbook ucs.yaml
    ;;
 
-7) echo "ansible-playbook iac.yaml -i inventory"
-   ansible-playbook iac.yaml -i inventory
+7) echo "ansible-playbook nxos25.yaml"
+   ansible-playbook nxos25.yaml
    ;;
+
+8) echo "ansible-playbook iac.yaml"
+   ansible-playbook iac.yaml
+   ;;
+
+vpn) echo "ansible-playbook vpn.yaml"
+   ansible-playbook vpn.yaml
+   ;;
+
+ucs) echo "ansible-playbook ucsm-sdk.yaml"
+   ansible-playbook ucsm-sdk.yaml
+   ;;
+
 
 esac
--- dnc2018/setup.sh
+++ dnc2018/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Prepare environment for DEVNET1002 Lab
 
-openconnect dcloud-sjc-anyconnect.cisco.com -b --servercert sha256:8951b2d47d8edefcfa875e0bc253418fd6be7
-294a5dce76f071e6bad258d67fe --no-dtls -q -u v130user1 -p f98d8
+#vagrant up
+docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_vpn:1.0 /bin/sh
