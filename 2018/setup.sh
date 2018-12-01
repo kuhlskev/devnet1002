25b8cf7bc0d690557107e246080dddc4a4aa6ba1
kekuhls <kekuhls@kekuhls-m-70yv.attlocal.net>
2018-12-01 16:01:38 -0500

    updating the ansible container

--- 2018/setup.sh
+++ 2018/setup.sh
@@ -2,4 +2,5 @@
 # Prepare environment for DEVNET1002 Lab
 
 vagrant up
-docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_vpn:1.0 /bin/sh
+docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_host /bin/sh
+#docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_vpn:1.0 /bin/sh
