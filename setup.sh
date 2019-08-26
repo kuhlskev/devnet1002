a07b4dfddda329ed3af04b374275f20de7785496
Francois Caen <fracaen@cisco.com>
2019-08-25 23:55:38 -0700

    touch up post-cleanup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@
 
 vagrant up
 docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_host /bin/sh
-#docker run -it --rm --privileged -v$(pwd):/home/docker kuhlskev/ansible_vpn:1.0 /bin/sh
--- stop.sh
+++ stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Remove environment for DEVNET1002 Lab
 
-vagrant destroy -f
-docker rm -f `docker ps -a -q`
+vagrant halt -f
