f077388d265a60bd78855f05082c94cd316048e4
Francois Caen <fracaen@cisco.com>
2022-06-05 12:02:52 -0700

    removed vagrant-related content

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remove environment for DEVNET1002 Lab
-
-vagrant destroy -f
-docker rm -f `docker ps -a -q`
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Remove environment for DEVNET1002 Lab
-
-vagrant halt -f
